22c4cc86e4f6b70afd46a19efda1c51ca96b48ca
noctarius <me@noctarius.com>
2014-05-20 10:03:41 +0200

    First import

---
+++ src/main/java/com/hazelcast/examples/csv/ReaderHelper.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2008-2013, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.examples.csv;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IList;
+import com.hazelcast.core.IMap;
+import com.hazelcast.examples.model.Crime;
+import com.hazelcast.examples.model.Person;
+import com.hazelcast.examples.model.State;
+
+import java.io.InputStream;
+import java.util.List;
+
+public final class ReaderHelper {
+
+    private static final ClassLoader CLASS_LOADER = ReaderHelper.class.getClassLoader();
+
+    private ReaderHelper() {
+    }
+
+    public static void read(HazelcastInstance hazelcastInstance)
+            throws Exception {
+
+        readStates(hazelcastInstance);
+        readPeople(hazelcastInstance);
+        readCrimes(hazelcastInstance);
+    }
+
+    private static void readStates(HazelcastInstance hazelcastInstance)
+            throws Exception {
+
+        StateDataReader stateDataReader = new StateDataReader();
+        try (InputStream is = CLASS_LOADER.getResourceAsStream("state_table.csv")) {
+            IMap<Integer, State> statesMap = hazelcastInstance.getMap("states");
+            List<State> states = stateDataReader.read(is);
+            for (State state : states) {
+                statesMap.put(state.getId(), state);
+            }
+        }
+    }
+
+    private static void readPeople(HazelcastInstance hazelcastInstance)
+            throws Exception {
+
+        PersonDataReader personDataReader = new PersonDataReader();
+        try (InputStream is = CLASS_LOADER.getResourceAsStream("us-500.csv")) {
+            IList<Person> personsList = hazelcastInstance.getList("persons");
+            List<Person> persons = personDataReader.read(is);
+            personsList.addAll(persons);
+        }
+    }
+
+    private static void readCrimes(HazelcastInstance hazelcastInstance)
+            throws Exception {
+
+        CrimeDataReader crimeDataReader = new CrimeDataReader();
+        try (InputStream is = CLASS_LOADER.getResourceAsStream("CrimeStatebyState.csv")) {
+            IList<Crime> crimesList = hazelcastInstance.getList("crimes");
+            List<Crime> crimes = crimeDataReader.read(is);
+            crimesList.addAll(crimes);
+        }
+    }
+}
